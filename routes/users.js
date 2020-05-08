287d39afae2283412327353a16377df63b118724
ManarKaram <59695197+ManarKaram@users.noreply.github.com>
2020-05-08 23:48:19 +0200

    delete un-used routes

--- routes/users.js
+++ routes/users.js
@@ -41,9 +41,6 @@ router.post('/login', async (req, res, next) => {
 
 
     const { email, password } = req.body;
-    // const user = await User.findOne({ username, password }).populate('posts').exec()
-    // if (!user) throw new Error("invalid credentials");
-    //............................................................................//
     const user = await User.findOne({ email });
     if (!user) {
         throw CustomError(422, "invalid credentials");
@@ -58,41 +55,28 @@ router.post('/login', async (req, res, next) => {
     res.json({ user, token })
 });
 
-router.get('/chat', authenticationMiddleWare, (req, res, next) => {
-    res.send('User Token Verified')
-})
-router.delete('/', authenticationMiddleWare, async (req, res, next) => {
-    //  const token = req.headers.authorization;
-    //const currentUser = await User.getUserFromToken(token);
-    const id = req.user.id;
-    const user = await User.findByIdAndDelete(id);
-    res.status(200).json(user);
 
+// router.patch('/', authenticationMiddleWare,
+//     validationMiddleWare(
+//         check('password')
+//             .isLength({ min: 5 }).withMessage('must be at least 5 chars long')
+//             .matches(/\d/).withMessage('must contain a number'),
+//         check('username').isLength({ min: 5 }).withMessage('must be at least 5 chars long')
+//             .isEmail().withMessage('Password must be an email')
+//     ),
+//     async (req, res, next) => {
+//         const id = req.user.id;
+//         const { username, password, firstName, age } = req.body;
+//         const user = await User.findByIdAndUpdate(id, {
+//             username, password, firstName, age
+//         }, {
+//             new: true,
+//             runValidators: true,
+//             omitUndefined: true
+//         });
+//         res.status(200).json(user);
 
-
-});
-
-router.patch('/', authenticationMiddleWare,
-    validationMiddleWare(
-        check('password')
-            .isLength({ min: 5 }).withMessage('must be at least 5 chars long')
-            .matches(/\d/).withMessage('must contain a number'),
-        check('username').isLength({ min: 5 }).withMessage('must be at least 5 chars long')
-            .isEmail().withMessage('Password must be an email')
-    ),
-    async (req, res, next) => {
-        const id = req.user.id;
-        const { username, password, firstName, age } = req.body;
-        const user = await User.findByIdAndUpdate(id, {
-            username, password, firstName, age
-        }, {
-            new: true,
-            runValidators: true,
-            omitUndefined: true
-        });
-        res.status(200).json(user);
-
-    });
+//     });
 
 
 
